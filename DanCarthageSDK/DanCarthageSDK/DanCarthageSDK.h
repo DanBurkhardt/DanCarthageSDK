dd6b8a4f76126c2a1924fa3ea0dd54e36fae04dc
Giganom <dan@giganom.com>
2017-03-07 18:56:21 -0500

    Testing initial push to git repo

---
+++ DanCarthageSDK/DanCarthageSDK/DanCarthageSDK.h
@@ -0,0 +1,19 @@
+//
+//  DanCarthageSDK.h
+//  DanCarthageSDK
+//
+//  Created by Daniel Burkhardt on 3/7/17.
+//  Copyright © 2017 Giganom LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DanCarthageSDK.
+FOUNDATION_EXPORT double DanCarthageSDKVersionNumber;
+
+//! Project version string for DanCarthageSDK.
+FOUNDATION_EXPORT const unsigned char DanCarthageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DanCarthageSDK/PublicHeader.h>
+
+
---
+++ DanCarthageSDK/DanCarthageSDK/DanCarthageSDK.swift
@@ -0,0 +1,17 @@
+//
+//  DanCarthageSDK.swift
+//  DanCarthageSDK
+//
+//  Created by Daniel Burkhardt on 3/7/17.
+//  Copyright © 2017 Giganom LLC. All rights reserved.
+//
+
+import Foundation
+
+public class DanCarthageSDK{
+    
+    func logToConsole(msg: String){
+        print(msg)
+    }
+    
+}
